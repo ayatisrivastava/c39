d7dfef3abe1070306d525f52b4b6dc2c3c2c617a
ayatisrivastava <ayatisrivastava1906@gmail.com>
2020-10-12 16:36:04 +0530

    marking player

--- js/Game.js
+++ js/Game.js
@@ -70,7 +70,10 @@ class Game {
         cars[index-1].y = y;
 
         if (index === player.index){
-          cars[index - 1].shapeColor = "red";
+          stroke(10);
+          fill("red");
+          ellipse(x,y,60,60);
+          //cars[index - 1].shapeColor = "red";
           camera.position.x = displayWidth/2;
           camera.position.y = cars[index-1].y;
         }
@@ -86,7 +89,7 @@ class Game {
       player.update();
     }
 
-    if(player.distance > 3860){
+    if(player.distance > 3500){
       gameState = 2;
     }
    
